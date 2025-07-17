cfcd49726564d1e2a9b4c1badc93f0d00cd964a7
Joseph <jcortes11100@gmail.com>
2025-07-17 14:06:43 -0600

    added the route.js

---
+++ app/api/movies/route.js
@@ -0,0 +1,18 @@
+import prisma from "../../../lib/prisma";
+
+export async function GET() {
+  const movies = await prisma.movie.findMany();
+  return new Response(JSON.stringify(movies), { status: 200 });
+}
+
+export async function POST(request) {
+  const { title, actors, releaseYear } = await request.json();
+  const movie = await prisma.movie.create({
+    data: {
+      title,
+      actors,                  // array of strings
+      releaseYear: Number(releaseYear),
+    },
+  });
+  return new Response(JSON.stringify(movie), { status: 201 });
+}
