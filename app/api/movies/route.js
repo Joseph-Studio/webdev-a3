043b0b562169e8b7923d7df88987aebda432cf64
Joseph <jcortes11100@gmail.com>
2025-07-25 12:28:03 -0600

    added put and delete in route.js

--- app/api/movies/route.js
+++ app/api/movies/route.js
@@ -10,9 +10,30 @@ export async function POST(request) {
   const movie = await prisma.movie.create({
     data: {
       title,
-      actors,                  // array of strings
+      actors, // array of strings
       releaseYear: Number(releaseYear),
     },
   });
   return new Response(JSON.stringify(movie), { status: 201 });
+}
+
+export async function PUT(request) {
+  const { id, title, actors, releaseYear } = await request.json();
+  const movie = await prisma.movie.update({
+    where: { id },
+    data: {
+      title,
+      actors,
+      releaseYear: Number(releaseYear),
+    },
+  });
+  return new Response(JSON.stringify(movie), { status: 200 });
+}
+
+export async function DELETE(request) {
+  const { id } = await request.json();
+  await prisma.movie.delete({
+    where: { id },
+  });
+  return new Response(null, { status: 204 });
 }
